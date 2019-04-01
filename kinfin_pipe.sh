9fdbd9307f8f1b953a51ab4c23f4f2341325c7a4
BioJNO <JamieNeilOrr@gmail.com>
2019-04-01 16:25:16 +0100

    error handling kinfin pipeline script update.

--- kinfin_pipe.sh
+++ kinfin_pipe.sh
@@ -1,58 +1,93 @@
-cd /home/jo42324/pas-genomics/assembly_update/kinfin
+cd /home/jo42324/scratch/pas-genomics/finalassembly/kinfin_pipeline_test/contigs/
 
-mkdir sane_proteins/
+# Check for all executables ----------------------------------------------------------------------
+echo Checking for required executables in path ---------------------------------------------------
+for name in orthofinder kinfin interproscan.sh prodigal filter_fastas_before_clustering.py
+do 
+    path_to_ex=$(which "$name")
+    if [ -x "$path_to_ex" ] ; then
+        echo ""$name" is here: $path_to_ex"
+    else
+        echo "WARNING:"$name" not found! Please install it and/or add the directory to your path."
+        exit
+    fi
+done 2>/dev/null
 
-cd sane_proteins/
+# Check for panther data -------------------------------------------------------------------------
+path_to_interproscan=$(which interproscan.sh)
+DIR=$(dirname "${path_to_interproscan}")
+if [ -d "${DIR}"/data/panther/ ]; then
+    echo Panther data found
+else
+    echo Panther data not found. You may need to download it or \
+    make changes to the interproscan command further down
+fi 2>/dev/null
 
-~/kinfin/scripts/filter_fastas_before_clustering.py -f ../BCER.Bacillus_cereus.faa > BCER.Bacillus_cereus.sl.faa
+# for all contig fasta files in directory predict genes using prodigal ----------------------------
+echo Running prodigal on contig fasta files -------------------------------------------------------
+mkdir predicted_proteins
+for f in *.fna
+do
+    prodigal \
+    -a predicted_proteins/"${f/%.fna/.faa}" \
+    -o predicted_proteins/"${f/%.fna/.gbk}" \
+    -q -i "$f" || { echo 'Prodigal failed!' ; exit 1; }
+done
 wait
-~/kinfin/scripts/filter_fastas_before_clustering.py -f ../BTHU.Bacillus_thuringiensis.faa > BTHU.Bacillus_thuringiensis.sl.faa
-wait
-~/kinfin/scripts/filter_fastas_before_clustering.py -f ../PPEN.Pasteuria_penetrans.faa > PPEN.Pasteuria_penetrans.sl.faa
-wait
-~/kinfin/scripts/filter_fastas_before_clustering.py -f ../BSUB.Bacillus_subtilis.faa > BSUB.Bacillus_subtilis.sl.faa
-wait
-~/kinfin/scripts/filter_fastas_before_clustering.py -f ../CDIF.Clostridioides_difficile.faa > CDIF.Clostridioides_difficile.sl.faa
-wait
-~/kinfin/scripts/filter_fastas_before_clustering.py -f ../TVUL.Thermoactinomyces_vulgaris.faa > TVUL.Thermoactinomyces_vulgaris.sl.faa
-wait
+
+# filter predicted proteins -----------------------------------------------------------------------
+echo Filtering predicted proteins -----------------------------------------------------------------
+cd predicted_proteins/
+mkdir ../sane_proteins/
+for f in *.faa
+do 
+    filter_fastas_before_clustering.py \
+    -f "$f" > ../sane_proteins/"${f/%.faa/.sl.faa}"
+done
 
 # RASTtk can put pipe characters in the names which will mess up Kinfin further down the pipe
-sed -i -e 's/|/_/g' *.sl.faa
+# if using RASTtk remove pipe characters with sed:
+#sed -i -e 's/|/_/g' *.sl.faa
 
+# Run orthofinder ---------------------------------------------------------------------------------
+echo Running orthofinder on filtered proteins -----------------------------------------------------
 cd ..
-
 orthoday=$(date -d "$D" '+%b%d')
-
-orthofinder -t 16 -n orthofinderout -f sane_proteins/
+orthofinder -t 16 -n orthofinderout -f sane_proteins/ || { echo 'Orthofinder failed!' ; exit 1; }
 wait
 
+# Concatenate all protein predictions and remove asterisks ----------------------------------------
+echo Concatenating protein predictions and removing asterisks -------------------------------------
 cd sane_proteins/
+sed -i -e 's/*//g' *.faa
+cat *.faa > ../all_proteins.faa
 
-cat *.faa > all_proteins.faa
-wait
-
+# Run interproscan --------------------------------------------------------------------------------
+echo Running interproscan on all filtered proteins ------------------------------------------------
+cd ..
 mkdir interpro_output
-
 bash interproscan.sh \
-	-i all_proteins.faa \
-	-d interpro_output/ \
-	-dp \
-	-pa \
-	-t p \
-	--goterms \
-	--cpu 16 \
-	-f TSV
+    -i all_proteins.faa \
+    -d interpro_output/ \
+    -dp \
+    -pa \
+    -t p \
+    --goterms \
+    --cpu 16 \
+    -f TSV ||{ echo 'interproscan failed!' ; exit 1; }
 wait
 
+# Make a kinfin config file -----------------------------------------------------------------------
+echo Making a kinfin config file ------------------------------------------------------------------
 echo '#IDX,TAXON' > config.txt
 sed 's/: /,/g' Results_orthofinderout_$orthoday/WorkingDirectory/SpeciesIDs.txt | \
-	cut -f 1 -d"." \
-	>> config.txt
-
+    cut -f 1 -d"." \
+    >> config.txt
 wait
 
-~/kinfin/scripts/iprs2table.py -i interpro_output/all_proteins.faa.tsv \
+# Convert interproscan output to format for kinfin ------------------------------------------------
+echo Converting interproscan output for kinfin ----------------------------------------------------
+iprs2table.py -i interpro_output/all_proteins.faa.tsv \
 --domain_sources TIGRFAM,\
 SFLD,\
 SUPERFAMILY,\
@@ -68,24 +103,22 @@ ProSitePatterns,\
 Pfam,\
 ProDom,\
 MobiDBLite,\
-PIRSF 
-
-wait 
-
-mkdir fasta/
-mv *sl.faa fasta/
-
+PIRSF
 wait
 
+# Run kinfin -----------------------------------------------------------------------------------
+echo Running Kinfin ----------------------------------------------------------------------------
 kinfin -g Results_orthofinderout_$orthoday/Orthogroups.txt \
 -c config.txt \
 -s Results_orthofinderout_$orthoday/WorkingDirectory/SequenceIDs.txt \
 -p Results_orthofinderout_$orthoday/WorkingDirectory/SpeciesIDs.txt \
--a fasta \
--f functional_annotation.txt 
-
+-a sane_proteins \
+-f functional_annotation.txt || { echo 'KinFin failed!' ; exit 1; }
 wait 
 
+# Generate a funtional annotation of clusters file ---------------------------------------------
+echo Generating cluster funtional annotation file ----------------------------------------------
 cd kinfin_results/
-
-~/kinfin/scripts/functional_annotation_of_clusters.py all -f cluster_domain_annotation.IPR.txt -c TAXON/TAXON.cluster_summary.txt
+functional_annotation_of_clusters.py all \
+-f cluster_domain_annotation.IPR.txt \
+-c TAXON/TAXON.cluster_summary.txt
